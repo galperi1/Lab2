69b528e23e642890bc0c899f6afc0f16af894c6e
Greg <Greg@Spicyboi>
2018-02-28 18:19:15 -0500

    galperi@udel.edu

--- FlamingoBLL/src/main/java/pkgCore/Card.java
+++ FlamingoBLL/src/main/java/pkgCore/Card.java
@@ -3,13 +3,38 @@ import pkgEnum.*;
 public class Card implements Comparable {
 
 	//	TODO: Card has two attributes, eRank and eSuit, add these attributes
-	
+	private int eRank;
+	private int eSuit;
 	
 	//	TODO: Build a constructor for Card passing in eRank and eSuit
-	
+	public Card(int eRank, int eSuit)
+	{
+		this.eRank = eRank;
+		this.eSuit = eSuit;
+	}
 	
 	//	TODO: Add a public 'getter' method for eRank and eSuit.  
 	//			Add a private 'setter' method for eRank and eSuit
+	
+	public int getRank()
+	{
+		return this.eRank;
+	}
+	
+	public int getSuit();
+	{
+		return this.sSuit;
+	}
+	
+	public void setRank(int r)
+	{
+		this.eRank = r;
+	}
+	
+	public void setSuit(int s)
+	{
+		this.eSuit = s 
+	}
 	
 	@Override
 	public int compareTo(Object o) {
