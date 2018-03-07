e49131b45238b5f681cf72c674c2ed92a5252abd
Greg <Greg@Spicyboi>
2018-03-06 19:59:18 -0500

    galperi@udel.edu

--- FlamingoBLL/src/main/java/pkgCore/Deck.java
+++ FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -3,13 +3,24 @@ package pkgCore;
 public class Deck {
 
 	//	TODO: Add 'cards' attribute that is an ArrayList of Card
-	private ArrayList<Card> DeckCards = new ArrayList<Card>;
+	private ArrayList<Card> DeckCards = new ArrayList<Card>();
 	
 	
 	
 	//	TODO: Add a contructor that passes in the number of decks, and then populates
 	//			ArrayList<Card> with cards (depending on number of decks).
-	public Deck (
+	public Deck ()
+	{
+		for (eSuit eSuit: eSuit.values())
+		{
+			for (eRank eRank: eRank.values())
+			{
+				DeckCards.add(new Card(eRank, eSuit));
+			}
+		}
+		Collections.shuffle(DeckCards);
+		
+	}
 	
 	//			Example: Deck(1) will build one 52-card deck.  There are 52 different cards
 	//			2 clubs, 3 clubs... Ace clubs, 2 hearts, 3 hearts... Ace hearts, etc
